64542e4339c870cbac67a4cf2cbe79362a5d09e4
moreirajonatan1983 <moreirajonatan1983@gmail.com>
2021-11-01 00:02:58 -0300

    modifico User

--- api-gw/src/main/java/org/utn/frd/dds/etp/repository/UserRepository.java
+++ api-gw/src/main/java/org/utn/frd/dds/etp/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package org.utn.frd.dds.etp.repository;
 
-public class UserRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.utn.frd.dds.etp.entity.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, String> {
+
 }

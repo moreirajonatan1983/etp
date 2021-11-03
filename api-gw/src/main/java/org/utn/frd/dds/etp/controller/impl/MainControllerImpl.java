4df4bc5e22a3e39aaa79755682098c601e763c79
moreirajonatan1983 <moreirajonatan1983@gmail.com>
2021-11-02 23:16:23 -0300

    Agrego MainControllerImpl

---
+++ api-gw/src/main/java/org/utn/frd/dds/etp/controller/impl/MainControllerImpl.java
@@ -0,0 +1,34 @@
+package org.utn.frd.dds.etp.controller.impl;
+
+import com.etp.crud.controller.impl.CrudControllerImpl;
+import io.swagger.annotations.ApiOperation;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.utn.frd.dds.etp.controller.OrderController;
+import org.utn.frd.dds.etp.entity.Order;
+import org.utn.frd.dds.etp.service.impl.OrderServiceImpl;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("pin")
+public class MainControllerImpl extends CrudControllerImpl<Order, String> {
+
+	private static final Log log = LogFactory.getLog(MainControllerImpl.class);
+
+	@RequestMapping(value="", method= RequestMethod.GET)
+	@ResponseBody
+	String home() {
+
+		log.info("Active service!!");
+
+		return "Active service!!";
+	}
+
+}
